1795ff074c3e3e1e61cf2a0cb82f2ac8d347ce9c
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-27 17:23:46 -0600

    Se agregó contexto

---
+++ P01_2022-SG-650_2022-PM-650/Models/ReservasContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace P01_2022_SG_650_2022_PM_650.Models
+{
+    public class ReservasContext : DbContext
+    {
+        public ReservasContext(DbContextOptions<ReservasContext> options) : base(options){ }
+    }
+}
