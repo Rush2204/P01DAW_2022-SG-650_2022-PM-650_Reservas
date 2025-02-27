e05f4a90629394b4824f6d5ce353267d4a6d5b66
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-27 17:57:10 -0600

    Add del modelado de tablas usuarios y sucursales

---
+++ P01_2022-SG-650_2022-PM-650/Models/sucursales.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022_SG_650_2022_PM_650.Models
+{
+    public class sucursales
+    {
+        [Key]
+        public int id_sucursal { get; set; }
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+        public string administrador { get; set; }
+        public string numeroEspacio { get; set; }
+    }
+}
---
+++ P01_2022-SG-650_2022-PM-650/Models/usuarios.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace P01_2022_SG_650_2022_PM_650.Models
+{
+    public class usuarios
+    {
+        [Key]
+        public int id_usuario { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public string contrasena { get; set; }
+        public string rol { get; set; }
+    }
+}
