18e497eda7ce815018c7dd77f3141633f5c53844
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-27 17:26:53 -0600

    Add dependencias

--- P01_2022-SG-650_2022-PM-650/Program.cs
+++ P01_2022-SG-650_2022-PM-650/Program.cs
@@ -1,8 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using P01_2022_SG_650_2022_PM_650.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ReservasContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ReservasDbConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
