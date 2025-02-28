47ceec0d8f4ac1e30abf0be2adf960b43bee8249
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-27 18:08:06 -0600

    update ReservasContext

--- P01_2022-SG-650_2022-PM-650/Models/ReservasContext.cs
+++ P01_2022-SG-650_2022-PM-650/Models/ReservasContext.cs
@@ -4,6 +4,12 @@ namespace P01_2022_SG_650_2022_PM_650.Models
 {
     public class ReservasContext : DbContext
     {
-        public ReservasContext(DbContextOptions<ReservasContext> options) : base(options){ }
+        public ReservasContext(DbContextOptions<ReservasContext> options) : base(options)
+        { }
+
+        public DbSet<Usuarios> usuarios { get; set; }
+        public DbSet<Sucursales> sucursales { get; set; }
+        public DbSet<Reserva> reserva  { get; set; }
+        public DbSet<EspaciosParqueo> espaciosParqueo { get; set; }
     }
 }
