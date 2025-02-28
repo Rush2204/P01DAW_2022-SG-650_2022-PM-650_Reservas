2d54b554edd74ed6f953a43eb38d02b5d357e39e
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-27 18:08:28 -0600

    add controller de usuarios y sucursales

---
+++ P01_2022-SG-650_2022-PM-650/Controllers/sucursalesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace P01_2022_SG_650_2022_PM_650.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class sucursalesController : ControllerBase
+    {
+    }
+}
