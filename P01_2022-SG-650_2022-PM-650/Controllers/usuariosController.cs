29740984422d1e274d4132dba13cfc6ea5cd2309
Alejandra <alejandrasandoval4321@gmail.com>
2025-02-27 19:42:39 -0600

    CRUD de usuarios

--- P01_2022-SG-650_2022-PM-650/Controllers/usuariosController.cs
+++ P01_2022-SG-650_2022-PM-650/Controllers/usuariosController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using P01_2022_SG_650_2022_PM_650.Models;
 
 namespace P01_2022_SG_650_2022_PM_650.Controllers
 {
@@ -7,5 +9,102 @@ namespace P01_2022_SG_650_2022_PM_650.Controllers
     [ApiController]
     public class usuariosController : ControllerBase
     {
+        private readonly ReservasContext _ReservasContext;
+
+        public usuariosController(ReservasContext ReservasContext)
+        {
+            _ReservasContext = ReservasContext;
+        }
+
+        // Para poder ver todos los registros:
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Index()
+        {
+            try
+            {
+                List<Usuario> listadoUsuario = (from e in _ReservasContext.usuario select e).ToList();
+
+                if (listadoUsuario.Count == 0)
+                {
+                    return NotFound();
+                }
+                return Ok(listadoUsuario);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Ocurrió un error: {ex.Message}");
+            }
+        }
+
+        // Para agregar un nuevo registro:
+
+        [HttpPost]
+        [Route("Add")]
+
+        public IActionResult GuardarUsuario([FromBody] Usuario usuario)
+        {
+            try
+            {
+                _ReservasContext.usuario.Add(usuario);
+                _ReservasContext.SaveChanges();
+                return Ok(usuario);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        // Para actualizar un registro
+
+        [HttpPut]
+        [Route("actualizar/{id}")]
+
+        public IActionResult ActualizarUsuario(int id, [FromBody] Usuario usuarioModificar)
+        {
+            Usuario? usuarioActual = (from e in _ReservasContext.usuario where e.id_usuario == id select e).FirstOrDefault();
+
+            if (usuarioActual == null)
+            { return NotFound(); }
+
+            usuarioActual.nombre = usuarioModificar.nombre;
+            usuarioActual.correo = usuarioModificar.correo;
+            usuarioActual.telefono = usuarioModificar.telefono;
+            usuarioActual.contrasena = usuarioModificar.contrasena;
+            usuarioActual.rol = usuarioModificar.rol;
+
+            _ReservasContext.Entry(usuarioActual).State = EntityState.Modified;
+            _ReservasContext.SaveChanges();
+
+            return Ok(usuarioModificar);
+        }
+
+        // Para Eliminar un registro
+
+        [HttpDelete]
+        [Route("eliminar/{id}")]
+
+        public IActionResult EliminarUsuario(int id)
+        {
+            Usuario? usuario = (from e in _ReservasContext.usuario where e.id_usuario == id select e).FirstOrDefault();
+
+            if (usuario == null)
+            { return NotFound(); }
+
+            _ReservasContext.usuario.Attach(usuario);
+            _ReservasContext.usuario.Remove(usuario);
+            _ReservasContext.SaveChanges();
+
+            return Ok(usuario);
+        }
+
+        //
+
+        
+
+
     }
 }
+
