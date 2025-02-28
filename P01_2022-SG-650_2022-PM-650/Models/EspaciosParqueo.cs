8cc61e3c7db165693a6d6aaf7ce226594b613c63
victo <victo@VICSLAPTOP>
2025-02-27 18:03:25 -0600

    Modelos Espacios y reservas

---
+++ P01_2022-SG-650_2022-PM-650/Models/EspaciosParqueo.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace P01_2022_SG_650_2022_PM_650.Models
+{
+    public class EspaciosParqueo
+    {
+        [Key]
+        public int id_espacio { get; set; }
+
+        public int id_sucursal { get; set; }
+
+        public int numero { get; set; }
+        public string? ubicacion { get; set; }
+
+        public decimal costoHora { get; set; }
+        public bool estado { get; set; } // true = Ocupado, false = Disponible
+    }
+}
---
+++ P01_2022-SG-650_2022-PM-650/Models/Reserva.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace P01_2022_SG_650_2022_PM_650.Models
+{
+    public class Reserva
+    {
+        [Key]
+        public int id_reserva { get; set; }
+        public int id_usuario { get; set; }
+        public int id_espacio { get; set; }
+        public Date? fecha { get; set; }
+        public TimeSpan horaInicio { get; set; }
+        public int cantidadHoras { get; set; }
+        public bool Estado { get; set; } // true = Activa, false = Cancelada
+    }
+}
